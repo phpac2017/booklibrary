a4ac5b36de85e74590a24afa2b70629f1fb6b6b9
phpac2015 <phpac2015@gmail.com>
2018-06-24 12:00:31 +0530

    Files Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,54 +13,6 @@
         <link href="{{ asset('css/lib.css') }}" rel="stylesheet">
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-                background: #34d240;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-                color: #e0ef09;
-            }
-
-            .links > a {
-                color: #fff;
-                padding: 0 30px;
-                /* height: 35px; */
-                text-align: center;
-                vertical-align: middle;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
